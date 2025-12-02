814be7a27d9adadb020b2c65412c2945373f1e66
swayamshreemohanty <swayamshree.mohanty@serendipityspace.in>
2025-12-02 20:29:36 +0530

    1st commit

---
+++ build/CMakeFiles/RunMe.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RunMe.dir/main.cpp.o"
+  "CMakeFiles/RunMe.dir/main.cpp.o.d"
+  "RunMe"
+  "RunMe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RunMe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello! CMake built this program successfully." << std::endl;
+    return 0;
+}
+
+
