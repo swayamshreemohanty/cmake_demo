7ffa287f2d33daf159a216df5e69e9508f9f5681
swayamshreemohanty <swayamshree.mohanty@serendipityspace.in>
2025-12-03 12:58:07 +0530

    Add .gitignore and remove build folder from tracking

--- build/MathBox/CMakeFiles/MyMathLib.dir/tools.cpp.o.d
+++
@@ -1,4 +0,0 @@
-MathBox/CMakeFiles/MyMathLib.dir/tools.cpp.o: \
- /home/swayamshree-mohanty/Documents/work/serendipityspace/cmake_demo/MathBox/tools.cpp \
- /usr/include/stdc-predef.h \
- /home/swayamshree-mohanty/Documents/work/serendipityspace/cmake_demo/MathBox/tools.h
